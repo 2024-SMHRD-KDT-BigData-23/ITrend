50315238a74cf32fd0823cac1c54af599d37996c
오인영 <155136608+oin1230@users.noreply.github.com>
2024-06-13 16:11:57 +0900

    수정.1

--- myapp/src/Component/Header.js
+++ myapp/src/Component/Header.js
@@ -7,58 +7,52 @@ import PrivateRoute from '../PrivateRoute';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faChartSimple, faUser } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
-import Modal from 'react-modal';
 
 const Header = () => {
 
     const [modalOpen, setModalOpen] = useState(false);
 
-    const openModal = () => {
-        setModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setModalOpen(false);
-    };
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
 
     return (
-        <header id='header' className='mainHeader'>
-            <h1>
-                <a href='#'>
-                    <img src='/images/ITLOGO.png' alt='ITrend' />
-                </a>
-            </h1>
-            <nav>
-                <ul>
-                    <li>
-                        <button id="headerMap">
-                            <FontAwesomeIcon icon={faLocationDot} className='icon' />
-                            <span>지도 홈</span>
-                        </button>
-                    </li>
-                    <li>
-                        <button id="headerAnalysis">
-                            <FontAwesomeIcon icon={faChartSimple} className='icon' />
-                            <span>데이터 분석</span>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-            <div id="user">
-                <button type="button" style={{ background: "none", border: "none" }} onClick={openModal}>
-                    <FontAwesomeIcon icon={faUser} className='icon' />
-                </button>
-            </div>
-            <Modal
-                isOpen={modalOpen}
-                onRequestClose={closeModal}
-                contentLabel="Login Modal"
-                className="Modal"
-                overlayClassName="Overlay"
-            >
-                <Login closeModal={closeModal} />
-            </Modal>
-        </header>
+        <div>
+            <header id='header' className='mainHeader'>
+                <h1>
+                    <a href='#'>
+                        <img src='/images/ITLOGO.png' alt='ITrend' />
+                    </a>
+                </h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <button id="headerMap">
+                                <FontAwesomeIcon icon={faLocationDot} className='icon' />
+                                <span>지도 홈</span>
+                            </button>
+                        </li>
+                        <li>
+                            <button id="headerAnalysis">
+                                <FontAwesomeIcon icon={faChartSimple} className='icon' />
+                                <span>데이터 분석</span>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+                <div id="user">
+                    <button type="button" style={{ background: "none", border: "none" }} onClick={openModal}>
+                        <FontAwesomeIcon icon={faUser} className='icon' />
+                    </button>
+                </div>
+            </header>
+            {modalOpen && (
+                <div className="modal-overlay2" onClick={closeModal}>
+                    <div className="modal-content2" onClick={(e) => e.stopPropagation()}>
+                        <Login closeModal={closeModal} />
+                    </div>
+                </div>
+            )}
+        </div>
 
     );
 }
