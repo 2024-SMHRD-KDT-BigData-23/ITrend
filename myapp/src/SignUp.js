d419231823c0b376223e952d95820a6460ded044
오인영 <155136608+oin1230@users.noreply.github.com>
2024-06-14 11:13:33 +0900

    2024 06 14 11 13 수정

--- myapp/src/SignUp.js
+++ myapp/src/SignUp.js
@@ -3,10 +3,9 @@ import { faUser, faKey, faCalendarDays, faVenusMars, faHomeUser, faMobileScreenB
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Swal from "sweetalert2";
 import { useNavigate } from 'react-router-dom';
+import './SignUp.css';
 
-
-
-function Signup({ closeSignUp ,closeModal }) {
+function Signup({ closeSignUp, closeModal }) {
     const [inputValues, setInputValues] = useState({
         user_id: '',
         user_pw: '',
@@ -56,90 +55,110 @@ function Signup({ closeSignUp ,closeModal }) {
     };
 
     return (
-
-
-        <div style={{ flex: 1, display: "flex", flexDirection: "column", justifyContent: "center" }}>
+        <div className='signUpContainer'>
             <form onSubmit={handleSubmit}>
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_id" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faUser} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_id" className='labelContainer'>
+                        <FontAwesomeIcon icon={faUser} className='icon' />
                     </label>
-                    <input type="text" id="user_id" name="user_id" value={inputValues.user_id}
+                    <input
+                        type="text"
+                        id="user_id"
+                        name="user_id"
+                        value={inputValues.user_id}
                         onChange={handleInputChange}
                         placeholder='아이디'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_pw" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faKey} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_pw" className='labelContainer'>
+                        <FontAwesomeIcon icon={faKey} className='icon' />
                     </label>
-                    <input type="password" id="user_pw" name="user_pw" value={inputValues.user_pw}
+                    <input
+                        type="password"
+                        id="user_pw"
+                        name="user_pw"
+                        value={inputValues.user_pw}
                         onChange={handleInputChange}
                         placeholder='비밀번호'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_name" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faUser} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_name" className='labelContainer'>
+                        <FontAwesomeIcon icon={faUser} className='icon' />
                     </label>
-                    <input type="text" id="user_name" name="user_name" value={inputValues.user_name}
+                    <input
+                        type="text"
+                        id="user_name"
+                        name="user_name"
+                        value={inputValues.user_name}
                         onChange={handleInputChange}
                         placeholder='이름'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_birthdate" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faCalendarDays} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_birthdate" className='labelContainer'>
+                        <FontAwesomeIcon icon={faCalendarDays} className='icon' />
                     </label>
-                    <input type="date" id="user_birthdate" name="user_birthdate" value={inputValues.user_birthdate}
+                    <input
+                        type="date"
+                        id="user_birthdate"
+                        name="user_birthdate"
+                        value={inputValues.user_birthdate}
                         onChange={handleInputChange}
                         placeholder='생년월일'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_gender" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faVenusMars} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_gender" className='labelContainer'>
+                        <FontAwesomeIcon icon={faVenusMars} className='icon' />
                     </label>
-                    <input type="text" id="user_gender" name="user_gender" value={inputValues.user_gender}
+                    <input
+                        type="text"
+                        id="user_gender"
+                        name="user_gender"
+                        value={inputValues.user_gender}
                         onChange={handleInputChange}
                         placeholder='성별'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_loc" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faHomeUser} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_loc" className='labelContainer'>
+                        <FontAwesomeIcon icon={faHomeUser} className='icon' />
                     </label>
-                    <input type="text" id="user_loc" name="user_loc" value={inputValues.user_loc}
+                    <input
+                        type="text"
+                        id="user_loc"
+                        name="user_loc"
+                        value={inputValues.user_loc}
                         onChange={handleInputChange}
                         placeholder='주소'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-
-                <div style={{ display: "flex", border: "1px solid #A7E6FF", height: "40px" }}>
-                    <label htmlFor="user_phone" style={{ width: "30%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <FontAwesomeIcon icon={faMobileScreenButton} style={{ color: "#A7E6FF" }} />
+                <div className='inputGroup'>
+                    <label htmlFor="user_phone" className='labelContainer'>
+                        <FontAwesomeIcon icon={faMobileScreenButton} className='icon' />
                     </label>
-                    <input type="text" id="user_phone" name="user_phone" value={inputValues.user_phone}
+                    <input
+                        type="text"
+                        id="user_phone"
+                        name="user_phone"
+                        value={inputValues.user_phone}
                         onChange={handleInputChange}
                         placeholder='연락처'
-                        style={{ flex: 1, border: "none", borderLeft: "1px solid #A7E6FF" }}
+                        className='inputField'
                     />
                 </div>
-
-                <button type="submit" style={{ width: "100%", height: "40px", background: "none", backgroundColor: "#A7E6FF", border: "1px solid #A7E6FF" }}>회원가입</button>
+                <button type="submit" className='submitButton'>회원가입</button>
             </form>
-            <div style={{ display: "flex", justifyContent: "end", width: "100%", height: "30px" }}>
-                <button onClick={closeSignUp} style={{ border: "none", background: "none", cursor: "pointer", color: "blue", textDecoration: "underline" }}>돌아가기</button>
+            <div className='backButtonContainer'>
+                <button onClick={closeSignUp} className='backButton'>돌아가기</button>
             </div>
         </div>
     );
